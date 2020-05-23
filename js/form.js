f977ca8a17e54c30bc5404e8a8c914f36a605279
Hector Liceaga <lice2187@gmail.com>
2020-05-22 21:56:28 -0300

    funcion agregar comentario

--- js/form.js
+++ js/form.js
@@ -1,2 +1,13 @@
 'use strict';
 
+
+msj = document.getElementById("msj");
+document.getElementById("btn").addEventListener("click", comentBelow(msj));
+
+function comentBelow(msj) {
+    var newDiv = document.createElement("div"); 
+    var newContent = document.createTextNode(msj); 
+    newDiv.appendChild(newContent); //añade texto al div creado. 
+    var currentDiv = document.getElementById("formulario"); 
+    document.body.insertBefore(currentDiv, newDiv); 
+}
